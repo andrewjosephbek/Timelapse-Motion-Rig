55f9a1109592c10b99899684111f165679efe00b
Andrew Josephbek <andrewjosephbek@gmail.com>
2025-07-28 17:49:07 -0700

    Working editable UI fields
    working data editing, split ino file into 2

--- Code/displaytest/displaytest.ino
+++ Code/displaytest/displaytest.ino
@@ -5,8 +5,8 @@
 #define ENCODER_CLK 34
 #define ENCODER_DT  35
 #define BUTTON_PIN 23
-#define DEBOUNCE_DELAY 50  // milliseconds
-
+#define BUTTON_DEBOUNCE_MILLIS 50  // milliseconds
+#define ENC_DEBOUNCE_MICRO 100
 #define N_DISPLAY_ELEMENTS 4
 #define TITLE_X_POS 0
 #define DATA_X_POS 90
@@ -15,9 +15,12 @@
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/SCL_PIN, /* data=*/SDA_PIN);
 
 // rotary encoder handling
-volatile unsigned int encoderPos = 1 << 31;
-volatile bool lastClkState = LOW;
+volatile unsigned int rawEncoderPos = 1 << 31;
 volatile unsigned long lastInterruptTime = 0;
+volatile uint8_t encoderPrevState = 0;
+
+int safePrevEncoderPos = rawEncoderPos/4;
+int encoderChange = 0;
 
 // rotary encoder button handling 
 bool lastButtonState = HIGH;
@@ -52,10 +55,7 @@ int axis2deg = 90;
 int nShots = 1000;
 int nDurationMins = 300;
 
-const int * timelapseParams[] = {&axis1deg, &axis2deg, &nShots, &nDurationMins};
-
-
-
+int * timelapseParams[] = {&axis1deg, &axis2deg, &nDurationMins, &nShots};
 
 void setup() {
   Serial.begin(115200);
@@ -67,81 +67,26 @@ void setup() {
   pinMode(ENCODER_DT, INPUT);
   pinMode(BUTTON_PIN, INPUT);
 
-  attachInterrupt(digitalPinToInterrupt(ENCODER_CLK), handleEncoder, CHANGE);
+attachInterrupt(digitalPinToInterrupt(ENCODER_CLK), handleEncoder, CHANGE);
+attachInterrupt(digitalPinToInterrupt(ENCODER_DT),  handleEncoder, CHANGE);
 }
 
 void loop() {
-  static int lastPos = 0;
+  unsigned int safeEncoderPos = 0;
+
+  noInterrupts();
+  safeEncoderPos = rawEncoderPos;
+  interrupts();
+
+  safeEncoderPos /= 4;
+  encoderChange = safeEncoderPos - safePrevEncoderPos;
 
   handleButton();
 
-  //Serial.println(digitalRead(BUTTON_PIN));
-
-  if (encoderPos != lastPos)
-    lastPos = encoderPos;
-
   u8g2.clearBuffer();
-
-  drawUI();
-
+  drawUI(safeEncoderPos, encoderChange);
   u8g2.sendBuffer();
 
-}
+  safePrevEncoderPos = safeEncoderPos;
 
-void drawUI() {
-
-  // draw suptitle
-  //Serial.println(encoderPos);
-  u8g2.setFont(u8g2_font_tinytim_tf);
-  u8g2.drawStr(0, 5, "CONFIGURE TIMELAPSE");
-  u8g2.drawLine(0, 6, 120, 6);
-
-  char buffer[5];
-
-  for (int i = 0; i < N_DISPLAY_ELEMENTS; i++) {
-    int j = (encoderPos - i + N_DISPLAY_ELEMENTS) % N_DISPLAY_ELEMENTS;
-    int y_pos = menuLineYPos[i];
-
-    // draw line titles
-    u8g2.setFont(menuLineFonts[i]);
-    u8g2.drawStr(TITLE_X_POS, y_pos, menuLinesTitles[j]);
-
-    // draw line params/data-vals
-    snprintf(buffer, sizeof(buffer), "%d", *timelapseParams[j]);
-    u8g2.drawStr(DATA_X_POS, y_pos, buffer);
-  }
-}
-
-void handleButton() {
-  int reading = digitalRead(BUTTON_PIN);
-
-  if ((millis() - lastDebounceTime) > DEBOUNCE_DELAY) {
-    if (reading == LOW && lastButtonState == HIGH) {
-      uiScroll = !uiScroll;
-      lastDebounceTime = millis();
-    }
-  }
-  lastButtonState = reading;
-}
-
-void IRAM_ATTR handleEncoder() {
-  unsigned long now = micros();
-  if (now - lastInterruptTime < 100) return; // debounce
-  
-  if (!uiScroll) return;
-
-  lastInterruptTime = now;
-
-  bool clkState = digitalRead(ENCODER_CLK);
-  bool dtState  = digitalRead(ENCODER_DT);
-
-  if (clkState != lastClkState && clkState == HIGH) {
-    if (dtState == clkState) {
-      encoderPos++;
-    } else {
-      encoderPos--; 
-    }
-  }
-
-  lastClkState = clkState;
 }
---
+++ Code/displaytest/function.ino
@@ -0,0 +1,79 @@
+
+void drawUI(int encoderPos, int encoderChange) {
+
+  static unsigned int uiState = 1 << 31;
+
+  // draw suptitle
+  //Serial.println(encoderPos);
+  u8g2.setFont(u8g2_font_tinytim_tf);
+  u8g2.drawStr(0, 5, "CONFIGURE TIMELAPSE");
+  u8g2.drawLine(0, 6, 125, 6);
+
+  char buffer[5];
+
+  if(uiScroll) {
+    uiState += encoderChange;
+  }
+
+  for (int i = 0; i < N_DISPLAY_ELEMENTS; i++) {
+    int j = ((uiState - i) % N_DISPLAY_ELEMENTS + N_DISPLAY_ELEMENTS) % N_DISPLAY_ELEMENTS;
+    int y_pos = menuLineYPos[i];
+
+    if(!uiScroll && i == 1)
+      *timelapseParams[j] += 10 * encoderChange;
+
+    // draw line titles
+    u8g2.setFont(menuLineFonts[i]);
+    u8g2.drawStr(TITLE_X_POS, y_pos, menuLinesTitles[j]);
+
+    // draw data vals
+    snprintf(buffer, sizeof(buffer), "%d", *timelapseParams[j]);
+    u8g2.drawStr(DATA_X_POS, y_pos, buffer);
+  }
+}
+
+
+
+void handleButton() {
+  bool reading = digitalRead(BUTTON_PIN);
+
+  if ((millis() - lastDebounceTime) > BUTTON_DEBOUNCE_MILLIS) {
+    if (reading == LOW && lastButtonState == HIGH) {
+      uiScroll = !uiScroll;
+      lastDebounceTime = millis();
+    }
+  }
+  lastButtonState = reading;
+}
+
+void IRAM_ATTR handleEncoder() {
+  static uint8_t lastState = 0;
+  uint8_t currentState = (digitalRead(ENCODER_CLK) << 1) | digitalRead(ENCODER_DT);
+
+  // Form a 4-bit value: prev_state << 2 | curr_state
+  uint8_t stateTransition = (lastState << 2) | currentState;
+
+  // Use a lookup table to determine direction
+  // Valid transitions (CW or CCW)
+  switch (stateTransition) {
+    case 0b0001:
+    case 0b0111:
+    case 0b1110:
+    case 0b1000:
+      rawEncoderPos++;
+      break;
+
+    case 0b0010:
+    case 0b0100:
+    case 0b1101:
+    case 0b1011:
+      rawEncoderPos--;
+      break;
+
+    default:
+      // ignore invalid transitions (could be bounce)
+      break;
+  }
+
+  lastState = currentState;
+}
